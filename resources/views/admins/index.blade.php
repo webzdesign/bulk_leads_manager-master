6ed4af690bc3bf21b5af4563367aea1e0319c752
ravi patel <dev@clicklearn.com>
2022-07-11 15:16:34 +0530

    admin page design  changes

--- resources/views/admins/index.blade.php
+++ resources/views/admins/index.blade.php
@@ -1,28 +1,9 @@
 @extends('layouts.master')
 @section('content')
     <div class="middleContent">
-        <div class="cards">
-            <div class="cardsHeader d-flex justify-content-end">
-
-                <button id="showModel" class="btn-primary f-500 f-14 float-right align-right" data-bs-toggle="modal"
-                    data-bs-target="#addAdmin">
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="#ffffff" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M8.00008 13.3332V7.99984M8.00008 7.99984V2.6665M8.00008 7.99984H13.3334M8.00008 7.99984H2.66675"
-                            stroke="#ffffff" stroke-width="2" stroke-linecap="round" />
-                        <defs>
-                            <linearGradient id="paint0_linear_1524_12120" x1="8.00008" y1="2.6665" x2="8.00008"
-                                y2="13.3332" gradientUnits="userSpaceOnUse">
-                                <stop stop-color="#ffffff" />
-                                <stop offset="1" stop-color="#ffffff" />
-                            </linearGradient>
-                        </defs>
-                    </svg>
-                    Add New Admin
-                </button>
-            </div>
-            <div class="card-body  table-responsive">
-                <table class="datatable  table table-bordered table-hover">
+            
+            <div class="cards">
+                <table class="table datatable" id="example" style="width:100%">
                     <thead>
                         <tr>
                             <th>First Name</th>
@@ -32,11 +13,63 @@
                         </tr>
                     </thead>
                     <tbody>
+                        <tr>
+                            <td class="c-7b">John</td>
+                            <td class="c-7b">Doe</td>
+                            <td class="c-16">john@gmail.com</td>
+                            <td class="tableCards">
+                                <div class="editDlbtn d-flex">
+                                    <a href="javascript:;">
+                                        <button class="editBtn">
+                                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path
+                                                    d="M13.8067 4.69354C14.0667 4.43354 14.0667 4.0002 13.8067 3.75354L12.2467 2.19354C12 1.93354 11.5667 1.93354 11.3067 2.19354L10.08 3.41354L12.58 5.91354L13.8067 4.69354ZM2 11.5002V14.0002H4.5L11.8733 6.6202L9.37333 4.1202L2 11.5002Z"
+                                                    fill="white" />
+                                            </svg>
+                                        </button>
+                                    </a>
+                                    <button class="deleteBtn">
+                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M12.6666 2.66667H10.3333L9.66659 2H6.33325L5.66659 2.66667H3.33325V4H12.6666V2.66667ZM3.99992 12.6667C3.99992 13.0203 4.14039 13.3594 4.39044 13.6095C4.64049 13.8595 4.97963 14 5.33325 14H10.6666C11.0202 14 11.3593 13.8595 11.6094 13.6095C11.8594 13.3594 11.9999 13.0203 11.9999 12.6667V4.66667H3.99992V12.6667Z"
+                                                fill="white" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="c-7b">John</td>
+                            <td class="c-7b">Doe</td>
+                            <td class="c-16">john@gmail.com</td>
+                            <td class="tableCards">
+                                <div class="editDlbtn d-flex">
+                                    <a href="javascript:;">
+                                        <button class="editBtn">
+                                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path
+                                                    d="M13.8067 4.69354C14.0667 4.43354 14.0667 4.0002 13.8067 3.75354L12.2467 2.19354C12 1.93354 11.5667 1.93354 11.3067 2.19354L10.08 3.41354L12.58 5.91354L13.8067 4.69354ZM2 11.5002V14.0002H4.5L11.8733 6.6202L9.37333 4.1202L2 11.5002Z"
+                                                    fill="white" />
+                                            </svg>
+                                        </button>
+                                    </a>
+                                    <button class="deleteBtn">
+                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M12.6666 2.66667H10.3333L9.66659 2H6.33325L5.66659 2.66667H3.33325V4H12.6666V2.66667ZM3.99992 12.6667C3.99992 13.0203 4.14039 13.3594 4.39044 13.6095C4.64049 13.8595 4.97963 14 5.33325 14H10.6666C11.0202 14 11.3593 13.8595 11.6094 13.6095C11.8594 13.3594 11.9999 13.0203 11.9999 12.6667V4.66667H3.99992V12.6667Z"
+                                                fill="white" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
-
-        </div>
     </div>
 
     <div class="modal fade fieldsModal groupModal" id="addAdmin">
@@ -158,10 +191,37 @@
 
 @section('script')
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script type="text/html" id="filterDropdown">
+        <div class="d-flex align-items-center filterPanelbtn">
+            <button id="showModel" class="btn-primary f-500 f-14 float-right align-right" data-bs-toggle="modal"
+                data-bs-target="#addAdmin">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="#ffffff" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M8.00008 13.3332V7.99984M8.00008 7.99984V2.6665M8.00008 7.99984H13.3334M8.00008 7.99984H2.66675"
+                        stroke="#ffffff" stroke-width="2" stroke-linecap="round" />
+                    <defs>
+                        <linearGradient id="paint0_linear_1524_12120" x1="8.00008" y1="2.6665" x2="8.00008"
+                            y2="13.3332" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#ffffff" />
+                            <stop offset="1" stop-color="#ffffff" />
+                        </linearGradient>
+                    </defs>
+                </svg>
+                Add New Admin
+            </button>
+        </div>
+    </script>
+
+    <script type="text/html" id="searchPannel">
+        <input class="form-control f-14" placeholder="Search here">
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.6932 14.2957L10.7036 9.31023C11.386 8.35146 11.791 7.1584 11.791 5.90142C11.791 2.64178 9.14704 0 5.8847 0C2.62254 0.00017572 0 2.64196 0 5.90142C0 9.16105 2.64397 11.8028 5.90631 11.8028C7.18564 11.8028 8.35839 11.3981 9.31795 10.7163L14.3076 15.7018C14.4994 15.8935 14.7553 16 15.0113 16C15.2672 16 15.523 15.8935 15.7149 15.7018C16.0985 15.2971 16.0985 14.6792 15.6935 14.2956L15.6932 14.2957ZM1.96118 5.90155C1.96118 3.72845 3.73104 1.98133 5.88465 1.98133C8.03826 1.9815 9.82938 3.72845 9.82938 5.90155C9.82938 8.07466 8.05952 9.82178 5.90591 9.82178C3.7523 9.82178 1.96118 8.05338 1.96118 5.90155Z" fill="#7B809A"/></svg>
+    </script>
+
     <script>
         $(document).ready(function() {
 
-            drawTable();
+            // drawTable();
 
             @if (Session::has('message'))
                 Swal.fire(
@@ -178,41 +238,62 @@
                     'error'
                 )
             @endif
+            
+            var datatable = $('#example').DataTable({
+                "dom":"<'filterHeader d-block-500 cardsHeader'<'#filterInput'><'#filterBtn'>>" + "<'row m-0'<'col-sm-12 p-0'tr>>" + "<'row datatableFooter'<'col-md-5 align-self-center'i><'col-md-7'p>>",
+                "ordering": false,
+                language: {
+                    search: "_INPUT_",
+                    searchPlaceholder: "Search here"
+                }
+            });
 
+            $('#filterInput').html($('#searchPannel').html());
 
-            function drawTable(param = {}) {
-                var datatable = $('.datatable').DataTable({
-                    destroy: true,
-                    processing: true,
-                    serverSide: true,
-                    pageLength: 10,
-                    searchable: false,
-                    ajax: {
-                        "url": "{{ route('admins.getData') }}",
-                        "dataType": "json",
-                        "type": "GET",
-                        "data": param,
-                    },
-                    columns: [{
-                            data: 'firstName',
+            $('#filterBtn').html($('#filterDropdown').html());
+            $('#filterInput > input').keyup(function(){
+                datatable.search($(this).val()).draw();
+            });
 
-                        },
-                        {
-                            data: 'lastName'
-                        },
-                        {
-                            data: 'email',
-                            searchable: true
-                        },
-                        {
-                            data: 'action',
-                            orderable: false,
-                            searchable: false
-                        },
+            // function drawTable(param = {}) {
+            //     var datatable = $('.datatable').DataTable({
+            //         destroy: true,
+            //         processing: true,
+            //         serverSide: true,
+            //         pageLength: 10,
+            //         searchable: false,
+            //         ajax: {
+            //             "url": "{{ route('admins.getData') }}",
+            //             "dataType": "json",
+            //             "type": "GET",
+            //             "data": param,
+            //         },
+            //         columns: [{
+            //                 data: 'firstName',
 
-                    ],
-                });
-            }
+            //             },
+            //             {
+            //                 data: 'lastName'
+            //             },
+            //             {
+            //                 data: 'email',
+            //                 searchable: true
+            //             },
+            //             {
+            //                 data: 'action',
+            //                 orderable: false,
+            //                 searchable: false
+            //             },
+
+            //         ],
+            //     });
+            //     $('#filterInput').html($('#searchPannel').html());
+
+            //     $('#filterBtn').html($('#filterDropdown').html());
+            //     $('#filterInput > input').keyup(function(){
+            //         datatable.search($(this).val()).draw();
+            //     });
+            // }
 
             $('#showModel').on('click', function(e) {
                 e.preventDefault();
