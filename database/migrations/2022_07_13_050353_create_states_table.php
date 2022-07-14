108771bc5317b6809989e818d9fe36ea3da05fbf
Vaidehi Choksi <choksiv.ap@gmail.com>
2022-07-14 12:21:57 +0530

    migration chnage

--- database/migrations/2022_07_13_050353_create_states_table.php
+++ database/migrations/2022_07_13_050353_create_states_table.php
@@ -15,7 +15,7 @@ class CreateStatesTable extends Migration
     {
         Schema::create('states', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('country_id')->constrained('countries');
+            $table->foreignId('country_id')->constrained('countries')->onDelete('cascade');;
             $table->string('name');
             $table->timestamps();
         });
