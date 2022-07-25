4ab20df1e62bf4d1ad088013bd8f57cbc93f6327
maulik <maulik@gmail.com>
2022-07-25 15:01:03 +0530

    import module - work in progress

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -107,6 +107,8 @@ class ImportController extends Controller
 
     public function start_upload(Request $request)
     {
+        ini_set('memory_limit', -1);
+        ini_set('MAX_EXECUTION_TIME', 0);
 
         $rows = FacadesExcel::toArray(new LeadImport,storage_path('app/import/' . $request->filename));
         $totalRows = count($rows[0]);
@@ -114,139 +116,151 @@ class ImportController extends Controller
         $duplicateRecords = 0;
 
         $email_id = LeadFields::where('columnName','email')->pluck('id');
-        $email_index = array_search($email_id[0],$request->id);
+        $email_index = array_search($email_id[0],$request->id) ? array_search($email_id[0],$request->id) : null;
 
         $gender_id = LeadFields::where('columnName','gender')->pluck('id');
-        $gender_index = array_search($gender_id[0],$request->id);
+        $gender_index = array_search($gender_id[0],$request->id) ? array_search($gender_id[0],$request->id) : null;
 
         $city_id = LeadFields::where('columnName','city_id')->pluck('id');
-        $city_index = array_search($city_id[0],$request->id);
+        $city_index = array_search($city_id[0],$request->id) ? array_search($city_id[0],$request->id) : null;
 
         $state_id = LeadFields::where('columnName','state_id')->pluck('id');
-        $state_index = array_search($state_id[0],$request->id);
+        $state_index = array_search($state_id[0],$request->id) ? array_search($state_id[0],$request->id) : null;
 
         $country_id = LeadFields::where('columnName','country_id')->pluck('id');
-        $country_index = array_search($country_id[0],$request->id);
+        $country_index = array_search($country_id[0],$request->id) ? array_search($country_id[0],$request->id) : null;
 
         $dob_id = LeadFields::where('columnName','birth_date')->pluck('id');
-        $dob_index = array_search($dob_id[0],$request->id);
-
+        $dob_index = array_search($dob_id[0],$request->id) ? array_search($dob_id[0],$request->id) : null;
 
         $invalid = 0;
         $repeatMail = [];
-        foreach($rows[0] as $data)
-        {
-            $emailArr[] = $data[$email_index];
-        }
-
         $emails = LeadDetail::all()->pluck('email')->toArray();
+        $lead = Lead::latest()->first()->id;
 
-        foreach($request->id as $column)
+        $columns = array_filter($request->id, fn($value) => !is_null($value) && $value !== 'null');
+        foreach($columns as $key => $column)
         {
-            $columnName[] = LeadFields::where('id',$column)->first()->columnName;
+            $columnName[$key] = LeadFields::where('id',$column)->first()->columnName;
         }
 
         $mainArr = [];
-        $lead = Lead::latest()->first()->id;
+
         foreach($rows[0] as $row) {
             $arr = [];
-           if( strtolower($row[$gender_index]) == 'm' || strtolower($row[$gender_index]) == 'male')
-           {
-                $row[$gender_index] = 1;
-           }
-           else if(strtolower($row[$gender_index]) == 'f' || strtolower($row[$gender_index]) == 'female')
-           {
-                $row[$gender_index] = 0 ;
-           }
-           else
-           {
+
+            if($gender_index) {
+                if( strtolower($row[$gender_index]) == 'm' || strtolower($row[$gender_index]) == 'male') {
                     $row[$gender_index] = 1;
-           }
-
-           $email = LeadDetail::where('email',$row[$email_index])->where('email','!=',null)->get()->count();
-           if($email)
-           {
-                $duplicateRecords ++;
-                $repeatMail[]= $row[$email_index];
-           }
-
-            $country = Country::where('name',$row[$country_index])->first();
-            if($country)
-            {
-                $row[$country_index] = intval($country['id']);
-            }
-            else
-            {
-                $country_data = Country::create([
-                    'name'=> $row[$country_index]
-                ]);
-                $row[$country_index] = $country_data->id;
+                } else if(strtolower($row[$gender_index]) == 'f' || strtolower($row[$gender_index]) == 'female') {
+                    $row[$gender_index] = 0 ;
+                } else {
+                    $row[$gender_index] = 1;
+                }
             }
 
-            $state = State::where('name',$row[$state_index])->first();
-            if($state)
-            {
-                $row[$state_index] = intval($state['id']);
-                $row[$country_index] = intval($state['country_id']);
-            }
-            else
-            {
-                $data = state::create([
-                    'name'=> $row[$state_index],
-                    'country_id' =>  $row[$country_index],
-                ]);
-                $row[$state_index] = $data->id;
+            if($email_index) {
+                $email = LeadDetail::where('email',$row[$email_index])->where('email','!=',null)->get()->count();
+                if($email) {
+                    $duplicateRecords ++;
+                    $repeatMail[]= $row[$email_index];
+                }
+
+                if( in_array($row[$email_index],$repeatMail) )
+                {
+                    $arr['is_duplicate'] = 1;
+                    $arr['is_invalid'] = 0;
+                }
+                elseif($row[$email_index] == null || $row[$email_index] == '' || !$row[$email_index])
+                {
+                    $arr['is_duplicate'] = 0;
+                    $arr['is_invalid'] = 1;
+                    $invalid++;
+                }
+                else
+                {
+                    $arr['is_duplicate'] = 0;
+                    $arr['is_invalid'] = 0;
+                }
             }
 
-            $city = City::where('name',$row[$city_index])->first();
-            if($city)
-            {
-                $row[$city_index] = intval($city['id']);
-                $row[$state_index] = intval($city['state_id']);
-            }
-            else
-            {
-                $data = City::create([
-                    'name'=> $row[$city_index],
-                    'state_id' => $row[$state_index]
-                ]);
-                $row[$city_index] = $data->id;
+            if($country_index) {
+                $country = Country::where('name',$row[$country_index])->first();
+                if($country) {
+                    $row[$country_index] = intval($country['id']);
+                } else {
+                    $country_data = Country::create([
+                        'name'=> $row[$country_index]
+                    ]);
+                    $row[$country_index] = $country_data->id;
+                }
             }
 
-            $arr = array_combine($columnName,$row);
+            if($state_index) {
+                $state = State::where('name',$row[$state_index])->first();
+                if($state) {
+                    $row[$state_index] = intval($state['id']);
+                    $row[$country_index] = intval($state['country_id']);
+                } else {
+                    if($country_index) {
+                        $data = state::create([
+                            'name'=> $row[$state_index],
+                            'country_id' =>  $row[$country_index],
+                        ]);
+                    } else {
+                        $data = state::create([
+                            'name'=> $row[$state_index],
+                            'country_id' =>  null,
+                        ]);
+                    }
+                    $row[$state_index] = $data->id;
+                }
+            }
 
-            if( in_array($row[$email_index],$repeatMail) )
-            {
-                 $arr['is_duplicate'] = 1;
-                 $arr['is_invalid'] = 0;
-            }
-            elseif($row[$email_index] == null || $row[$email_index] == '' || !$row[$email_index])
-            {
-                $arr['is_duplicate'] = 0;
-                $arr['is_invalid'] = 1;
-                $invalid++;
-            }
-            else
-            {
-                $arr['is_duplicate'] = 0;
-                $arr['is_invalid'] = 0;
+            if($city_index) {
+                $city = City::where('name',$row[$city_index])->first();
+                if($city) {
+                    $row[$city_index] = intval($city['id']);
+                    $row[$state_index] = intval($city['state_id']);
+                } else {
+                    if($state_index) {
+                        $data = City::create([
+                            'name'=> $row[$city_index],
+                            'state_id' => $row[$state_index]
+                        ]);
+                    } else {
+                        $data = City::create([
+                            'name'=> $row[$city_index],
+                            'state_id' => null
+                        ]);
+                    }
+                    $row[$city_index] = $data->id;
+                }
             }
 
-            $age = \Carbon\Carbon::parse($row[$dob_index])->diff(\Carbon\Carbon::now())->format('%y years');
+            if($dob_index) {
+                $age = \Carbon\Carbon::parse($row[$dob_index])->diff(\Carbon\Carbon::now())->format('%y years');
+                $arr['age'] = $age;
+            }
 
             $arr['lead_id'] = $lead;
-            $arr['age'] = $age;
-           array_push($mainArr,$arr);
 
+            foreach($columnName as $key => $column) {
+                $arr[$column] = $row[$key];
+            }
+            array_push($mainArr, $arr);
         }
 
         $imported = $totalRows - ($duplicateRecords+$invalid);
 
-        if(!empty($mainArr)){
-            LeadDetail::query()->insert($mainArr); //good, just be careful of the size limit of $arr, you may need to chunk it
+        if(!empty($mainArr)) {
+            foreach(array_chunk($mainArr, 1000) as $details) {
+                LeadDetail::query()->insert($details); //good, just be careful of the size limit of $arr, you may need to chunk it
+            }
         }
+
+
         if($rows) {
-            //redirect
             Lead::find($lead)->update([
                 'rows'=>$totalRows,
                 'duplicate_row'=>$duplicateRecords,
@@ -260,9 +274,8 @@ class ImportController extends Controller
             $from = \Carbon\Carbon::now();
 
             $uploadTime = $file . " (Uploaded " . $uploadedTime->diffInHours($from) .' hours and '. $uploadedTime->diffInMinutes($from).' minutes ago.)';
-
-
             return response()->json(['message'=>'Data Inserted successFully','duplicate'=>$duplicateRecords,'invalid' => $invalid ,'import'=>$imported,'rows'=>$totalRows ,'done'=>true ,'lead'=>$lead , 'uploadTime'=>$uploadTime]);
+
         } else {
             return response()->json(['message'=>'No Data Found In File','done'=>false]);
         }
---
+++ database/migrations/2022_07_25_065113_remove_foreign_key_from_cities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveForeignKeyFromCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            DB::statement('ALTER TABLE `cities` DROP INDEX `cities_state_id_foreign`');
+            $table->bigInteger('state_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            $table->foreign('state_id')->references('id')->on('states');
+        });
+    }
+}
