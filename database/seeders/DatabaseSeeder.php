d5b0fb8aa7f408646fd5561126ca25bac9d8b67f
Parth-i7 <parthbagiya.ap@gmail.com>
2022-07-25 18:37:55 +0530

    seeder change

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
         User::updateOrCreate(['email' => 'admin@gmail.com'],
         [
             'firstName' => 'Admin',
+            'lastName'  => 'Admin',
             'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('123456'),
@@ -30,6 +31,7 @@ class DatabaseSeeder extends Seeder
         User::updateOrCreate(['email' => 'admin@apacheleads.com'],
         [
             'firstName' => 'Admin apacheleads',
+            'lastName'  => 'Admin',
             'email' => 'admin@apacheleads.com',
             'email_verified_at' => now(),
             'password' => Hash::make('123456'),
@@ -40,6 +42,7 @@ class DatabaseSeeder extends Seeder
         User::updateOrCreate(['email' => 'admin@xsited.com'],
         [
             'firstName' => 'Admin xsited',
+            'lastName'  => 'Admin',
             'email' => 'admin@xsited.com',
             'email_verified_at' => now(),
             'password' => Hash::make('123456'),
@@ -50,6 +53,7 @@ class DatabaseSeeder extends Seeder
         User::updateOrCreate(['email' => 'shivendrasinhj.ap@gmail.com'],
         [
             'firstName' => 'Admin shivendrasinhj',
+            'lastName'  => 'Admin',
             'email' => 'shivendrasinhj.ap@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('123456'),
