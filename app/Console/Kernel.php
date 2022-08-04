05b78a8c620dadb34b47c3b40fac79af01c111e7
maulik <maulik@gmail.com>
2022-08-04 12:30:42 +0530

    kernel file changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('lead:delete')->daily();
+        $schedule->command('lead:delete')->daily()->withoutOverlapping();
         $schedule->command('lead:send')->everyFiveMinutes()->withoutOverlapping();
     }
 
