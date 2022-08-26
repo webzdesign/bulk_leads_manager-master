54a85f4d27a89b657e8906cdd698fae1277ca08b
Angels Pearl <angelspearlit@gmail.com>
2022-08-26 19:28:49 +0530

    Load issue revert

--- app/Http/Controllers/NewOrderController.php
+++ app/Http/Controllers/NewOrderController.php
@@ -145,7 +145,7 @@ class NewOrderController extends Controller
         }
     }
 
-    public function count_total_leads_available_bkp(Request $request){
+    public function count_total_leads_available(Request $request){
 
         $total_leads_available = 0;
         $LeadTypes = LeadType::find($request->lead_type_id);
@@ -208,7 +208,7 @@ class NewOrderController extends Controller
         return response()->json([true, ['total_leads_available' => $total_leads_available, 'LeadTypes' => $LeadTypes->name, 'qry' => $qry]]);
     }
 
-    public function count_total_leads_available(Request $request){
+    public function count_total_leads_available_new(Request $request){
 
         $checkExist = Lead::where('lead_type_id',$request->lead_type_id)->exists();
         if($checkExist){
