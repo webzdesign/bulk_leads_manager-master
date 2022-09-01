387c82254ef5aaedced88b68ba619244e6b454bf
maulik <maulik@gmail.com>
2022-09-01 10:42:29 +0530

    state add

--- app/Http/Controllers/NewOrderController.php
+++ app/Http/Controllers/NewOrderController.php
@@ -353,7 +353,7 @@ class NewOrderController extends Controller
         if($request->leadTypeId != '') {
 
             if($request->leadTypeId == 2) {
-                $array = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
+                $array = ['AL','AK','AZ','AR','CA','CO','CT','DC','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
             } else {
                 $array = ['ACT','NSW','NT','QLD','SA','VIC','TAS','WA'];
             }
